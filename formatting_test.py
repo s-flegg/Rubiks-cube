11a731061c6ceec5304e34aeccf41a17be45272c
s-flegg <161213652+s-flegg@users.noreply.github.com>
2024-11-26 11:54:09 +0000

    Delete formatting_test.py

--- formatting_test.py
+++
@@ -1,6 +0,0 @@
-# this is a pep8 iolaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaation
-test = "now how about thissssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
-class badTest():
-    fix = "hmmm?"
-
-print(badTest.fix)
