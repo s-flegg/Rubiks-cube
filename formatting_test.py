eb1bd22b0fca3f9ddfea33f4da860b55ddd8c02e
s-flegg <161213652+s-flegg@users.noreply.github.com>
2024-11-26 11:45:54 +0000

    Format testing

---
+++ formatting_test.py
@@ -0,0 +1,6 @@
+# this is a pep8 iolaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaation
+test = "now how about thissssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+class badTest():
+    fix = "hmmm?"
+
+print(badTest.fix)
